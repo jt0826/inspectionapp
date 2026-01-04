181d113dbb72e13aa04a00c492bdb0d3b2ab45d0
Javier <jav.teys@gmail.com>
2026-01-05 00:51:35 +0800

    commit

--- src/components/FadeInText.tsx
+++ src/components/FadeInText.tsx
@@ -8,7 +8,7 @@ interface FadeInTextProps {
 
 export function FadeInText({ children, visible = true, className = '' }: FadeInTextProps) {
   return (
-    <span className={`${className} transition-opacity transition-transform duration-1000 ${visible ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-1'}`}>
+    <span className={`${className} transition-opacity transition-transform duration-200 ${visible ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-1'}`}>
       {children}
     </span>
   );
